7faf254eaa05fe3259ee07039ba253a88e40a402
KingOfCactus <kingcactusgamedev@gmail.com>
2023-07-11 10:17:24 -0300

    Moved game logic and rendering to separate module
    Created game_states module for GameState structs;
    Moved gameloop logic to a update function;
    Moved game rendering to a draw function;

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,19 @@
 mod input_system;
 mod game_objects;
+mod game_states;
 mod utils;
 
-use crate::utils::*;
-use crate::game_objects::*;
-
-use rand::*;
-use raylib::prelude::*;
-use raylib::ffi::KeyboardKey::*;
+use game_states::*;
+use utils::*;
 
 fn main() {
-    let mut score = 0;
-    let mut show_stats = true;
-    let mut highscore = get_highscore(); 
-    
     let (mut rl, thread) = init_window();
-    let (mut player, mut left_paddle, mut right_paddle) = init_gobjects();
+    let mut active_state = GameLoop::new();
 
     // Each frame
     while !rl.window_should_close() {
-        // <-- GAME LOGIC -->
-        player.update(&rl);
-
-        left_paddle.player_pos = player.position;
-        left_paddle.update(&rl);
-
-        right_paddle.player_pos = player.position;
-        right_paddle.update(&rl);
-
-        // Restart if player is outside of the screen
-        if player.position.x > SCREEN_SIZE.x || player.position.x < 0.0 {
-
-            // Reset variables
-            player.position = SCREEN_SIZE / 2.0;
-            left_paddle.position.y =  SCREEN_SIZE.y / 2.0 - PADDLE_SIZE.y / 2.0;
-            right_paddle.position.y =  SCREEN_SIZE.y / 2.0 - PADDLE_SIZE.y / 2.0;
-
-            player.input.dir = Vector2::zero();
-            player.prone_dir = Vector2 { x: -1.0, y: 0.0 };
-
-            // Check for a new highscore
-            if score > highscore { 
-                save_highscore(score);
-                highscore = score;
-            }
-            else { score = 0 }
-
-        }
-
-        // Bounce when hit a paddle
-        let hit_paddle = left_paddle.hitbox.check_collision_circle_rec(player.position, player.radius + 5.0) ||
-                         right_paddle.hitbox.check_collision_circle_rec(player.position, player.radius + 5.0);
-        
-        if hit_paddle {
-            let mut new_angle: f32 = thread_rng().gen();
-
-            // Copy player.input direction or keep previous direction 
-            if player.input.raw_dir.y == 0.0 { new_angle *= player.prone_dir.y.signum(); }
-            else { new_angle *= player.input.raw_dir.y.signum(); }
-            
-            // Keep player out of the paddles
-            let min = left_paddle.position.x + PADDLE_SIZE.x + player.radius;
-            let max = right_paddle.position.x - PADDLE_SIZE.x - player.radius;
-            player.position = player.position.clamp(min, max);
-
-            // Set new direction
-            player.prone_dir.x *= -1.0;
-            player.prone_dir.y = new_angle;
-            player.input.dir = Vector2 { x: 0.0, y: 0.0 };
-
-            // Set new player stats
-            // player_velocity = VECTOR_ZERO;          
-            score += 1;
-        }
-
-        // Bounce when hit top or bottom screen
-        if player.position.y == player.radius || player.position.y == SCREEN_SIZE.y - player.radius {
-            let mut new_angle = player.prone_dir.y.abs();
-            new_angle = new_angle.clamp(0.6, 1.0);
-
-            new_angle *= -player.prone_dir.y.signum();
-            player.prone_dir.y = new_angle;
-            
-            player.input.dir.x *= 0.5;
-            player.input.dir.y = 0.0;
-        }
-
-        if rl.is_key_pressed(KEY_TAB) { show_stats = !show_stats; }
-        drawn_screen(&show_stats, &mut rl, &thread, &player, &left_paddle, &right_paddle, &highscore, &score);
+        active_state.update(&rl);
+        active_state.draw(&mut rl, &thread);
     }
 
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,17 +1,23 @@
-use crate::game_objects::*;
-
 use std::fs;
 use std::io::Write;
 use raylib::prelude::*;
 use raylib::prelude::Vector2;
 
-const PADDLE_PADDING: f32 = 20.0;
-const INITIAL_PADDLE_RANGE: f32 = 0.5;
-const INITIAL_PADDLE_SPEED: f32 = 500.0;
+pub const PADDLE_PADDING: f32 = 20.0;
+pub const INITIAL_PADDLE_RANGE: f32 = 0.5;
+pub const INITIAL_PADDLE_SPEED: f32 = 500.0;
 
 pub const PADDLE_SIZE: Vector2 = Vector2 { x: 11.0, y: 65.0 };
 pub const SCREEN_SIZE: Vector2 = Vector2 { x: 640.0, y: 480.0 };
 
+pub fn init_window() -> (RaylibHandle, RaylibThread) {
+    let (mut rl_handle, _thread) = raylib::init()
+        .size(SCREEN_SIZE.x as i32, SCREEN_SIZE.y as i32)
+        .title("Pong 2").vsync().build();
+
+    rl_handle.set_target_fps(60);
+    return (rl_handle, _thread);
+}
 
 pub fn get_highscore() -> i32 {
     match fs::read_to_string("highscore.txt") {
@@ -30,85 +36,4 @@ pub fn save_highscore(i: i32) {
     let mut file = fs::OpenOptions::new().write(true).open("highscore.txt").unwrap();
     let buffer: String = i.to_string();
     file.write_all(buffer.as_bytes()).unwrap();
-}
-
-
-pub fn init_window() -> (RaylibHandle, RaylibThread) {
-    let (mut rl_handle, _thread) = raylib::init()
-        .size(SCREEN_SIZE.x as i32, SCREEN_SIZE.y as i32)
-        .title("Pong 2").vsync().build();
-
-    rl_handle.set_target_fps(60);
-    return (rl_handle, _thread);
-}
-
-pub fn init_gobjects() -> (Ball, Paddle, Paddle) {
-        let player = Ball::new(
-            Vector2 { x: SCREEN_SIZE.x * 0.9, y: SCREEN_SIZE.y * 0.5 },
-            Color { r: 255, g: 255, b: 255, a: 185},
-            10.0,
-            500.0,
-        );
-
-        let left_paddle = Paddle::new(
-            Vector2 { 
-                x: PADDLE_PADDING, 
-                y: SCREEN_SIZE.y / 2.0 - PADDLE_SIZE.y / 2.0 
-            },
-            Color::WHITE, PADDLE_SIZE, 
-            INITIAL_PADDLE_SPEED,
-            INITIAL_PADDLE_RANGE
-        );
-
-        let right_paddle = Paddle::new(
-            Vector2 { 
-                x: SCREEN_SIZE.x - PADDLE_SIZE.x - PADDLE_PADDING, 
-                y: SCREEN_SIZE.y / 2.0 - PADDLE_SIZE.y / 2.0 
-            },
-            Color::WHITE, PADDLE_SIZE, 
-            INITIAL_PADDLE_SPEED,
-            INITIAL_PADDLE_RANGE
-        );
-
-        return (player, left_paddle, right_paddle);
-}
-
-
-pub fn get_debug_info(player: &Ball) -> String {
-    let input = &player.input;
-    let mut stats = format!("- Prone: ({:.2}, {:.2}) \n- Move: ({:.2}, {:.2}\n", 
-                    player.prone_dir.x, player.prone_dir.y, 
-                    input.dir.x, input.dir.y);
-
-    if player.input.on_gamepad {
-        stats += &format!(" {} \n ({:.2}, {:.2})", input.gamepad_name, 
-                 input.raw_dir.x, input.raw_dir.y);
-    }
-    
-    return stats;
-}
-
-pub fn drawn_screen (debug: &bool, rl: &mut RaylibHandle, thread: &RaylibThread, 
-                     player: &Ball, left_paddle: &Paddle, right_paddle: &Paddle,
-                     highscore: &i32, score: &i32) {
-    // Clear screen
-    let mut draw_handle = rl.begin_drawing(thread);
-    draw_handle.clear_background(Color::BLACK);
-
-    // Draw score text
-    let text = format!("Hiscore: {}\n Score: {}", highscore, score);
-    let centralized_x = SCREEN_SIZE.x / 2.0 - (measure_text(&text, 22) as f32 / 2.0);
-    draw_handle.draw_text(&text, centralized_x as i32, (SCREEN_SIZE.y * 0.01) as i32, 22, Color::RED);
-    
-    // Draw game objects
-    draw_handle.draw_circle_v(player.position, player.radius, player.color);
-    draw_handle.draw_rectangle_rec(&left_paddle.hitbox, Color::GRAY);
-    draw_handle.draw_rectangle_rec(&right_paddle.hitbox, Color::GRAY);
-
-    // Draw debug info
-    if *debug {
-        let stats = get_debug_info(&player);
-        draw_handle.draw_fps(0, 0);
-        draw_handle.draw_text(&stats, 0, (SCREEN_SIZE.y * 0.05) as i32, 18, Color::GREEN);
-    }
 }
